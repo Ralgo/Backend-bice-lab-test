d11d888f4507582515b60a66d5065f9892b24cf1
Cristian R Beroiza <crberoizar@sodimac.cl>
2021-03-11 22:40:21 -0300

    Uploading base spring io project

---
+++ src/main/java/cl/bice/lab/test/TestApplication.java
@@ -0,0 +1,13 @@
+package cl.bice.lab.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
